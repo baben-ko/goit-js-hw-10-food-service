7e9ff91faa32982fa5caff6444ef15ea157047d8
Andrii_Babenko <andrey.brnr@gmail.com>
2021-02-22 19:26:26 +0200

    Updates

--- src/js/templating.js
+++
@@ -1,6 +0,0 @@
-import cardsTemplate from '../templates/cards.hbs';
-import cards from '../menu.json';
-
-const markup = cardsTemplate(cards);
-const galleryRef = document.querySelector('.js-menu');
-galleryRef.insertAdjacentHTML('afterbegin', markup);
